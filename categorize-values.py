19ae4e2622c3a7e34b150d9fa66cd13ee6152924
VarshaAggarwal16 <99105290+VarshaAggarwal16@users.noreply.github.com>
2022-08-11 09:56:22 +0200

    Add files via upload

---
+++ categorize-values.py
@@ -0,0 +1,3 @@
+mymixedtypelist = (45 , 290578 , 1.02 , True , "my dog is on the bed", "45")
+for item in mymixedtypelist : print("{} is of data type {}" .format (item, type(item)))
+
---
+++ collections.py
@@ -0,0 +1,30 @@
+myfruitlist=["apple", "banana", "cherry"]
+print(myfruitlist)
+print(type(myfruitlist))
+
+
+print(myfruitlist[0])
+print(myfruitlist[1])
+print(myfruitlist[2])
+myfruitlist[2] = "orange"
+print(myfruitlist)
+
+
+myfinalanswertuple = ("apple", "banana", "pineapple")
+print(myfinalanswertuple)
+print(type(myfinalanswertuple))
+
+
+print(myfinalanswertuple[0])
+print(myfinalanswertuple[1])
+print(myfinalanswertuple[2])
+
+
+myfavoritefruitdictionary = {"Akua" : "apple", "saanvi" : "banana", "paulo" : "pineapple"}
+print(myfavoritefruitdictionary)
+print(type(myfavoritefruitdictionary))
+
+
+print(myfavoritefruitdictionary["Akua"])
+print(myfavoritefruitdictionary["saanvi"])
+print(myfavoritefruitdictionary["paulo"])
